3fc97904d7f89bc0448c41773eaa2e40373ccd59
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-08-29 14:24:49 +0530

    style class is added for both done and not done, stricking and other css has to add

--- src/Task.js
+++ src/Task.js
@@ -4,10 +4,22 @@ class Task extends React.Component {
   constructor(props) {
     super(props);
     this.state = { done: false };
+    this.handleOnCLick = this.handleOnCLick.bind(this);
+  }
+
+  handleOnCLick(event) {
+    this.setState((prevState) => ({ done: !prevState.done }));
   }
 
   render() {
-    return <div>{this.props.task}</div>;
+    return (
+      <div
+        className={this.state.done ? 'finished' : 'not-finished'}
+        onClick={this.handleOnCLick}
+      >
+        {this.props.task}
+      </div>
+    );
   }
 }
 
