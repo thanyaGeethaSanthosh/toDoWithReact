a92de8146e883ae648a574c398c680b48c6cc114
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-09-02 15:45:51 +0530

    added hover in task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import CloseButton from './closeButton';
+import { prettyDOM } from '@testing-library/react';
 
-const Task = (props) => {
-  const { task, state, onClickHandler, id, deleteTask } = props;
-  const classList = {
-    todo: 'default-todo',
-    doing: 'doing-todo',
-    done: 'finished',
-  };
-  const className = classList[state];
-  return (
-    <div className='task-div'>
-      <div className={className} id={id} onClick={onClickHandler}>
-        {task}
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+    this.toggleHover = this.toggleHover.bind(this);
+  }
+  toggleHover() {
+    this.setState((prevState) => ({ hover: !prevState.hover }));
+  }
+  render() {
+    const { task, state, onClickHandler, id, deleteTask } = this.props;
+    const classList = {
+      todo: 'default-todo',
+      doing: 'doing-todo',
+      done: 'finished',
+    };
+    const className = classList[state];
+    return (
+      <div
+        className='task-div'
+        onMouseEnter={this.toggleHover}
+        onMouseLeave={this.toggleHover}
+      >
+        <div className={className} id={id} onClick={onClickHandler}>
+          {task}
+        </div>
+        {this.state.hover ? (
+          <CloseButton
+            onClick={() => {
+              deleteTask(id);
+            }}
+          />
+        ) : (
+          ''
+        )}
       </div>
-      <CloseButton
-        onClick={() => {
-          deleteTask(id);
-        }}
-      />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Task;
