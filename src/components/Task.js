2901e370294f250a4286bc2e224ec569cab45bc0
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-09-08 14:01:10 +0530

    converted TAsk to useState using useState

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,45 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CloseButton from './closeButton';
-import { prettyDOM } from '@testing-library/react';
 
-class Task extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hover: false };
-    this.toggleHover = this.toggleHover.bind(this);
-  }
-  toggleHover() {
-    this.setState((prevState) => ({ hover: !prevState.hover }));
-  }
-  render() {
-    const { task, state, onClickHandler, id, deleteTask } = this.props;
-    const classList = {
-      todo: 'default-todo',
-      doing: 'doing-todo',
-      done: 'finished',
-    };
-    const className = classList[state];
-    return (
-      <div
-        className='left-text flex'
-        onMouseEnter={this.toggleHover}
-        onMouseLeave={this.toggleHover}
-      >
-        <div className={className} id={id} onClick={onClickHandler}>
-          {task}
-        </div>
-        {this.state.hover ? (
-          <CloseButton
-            onClick={() => {
-              deleteTask(id);
-            }}
-          />
-        ) : (
-          ''
-        )}
+const Task = (props) => {
+  const { task, state, onClickHandler, id, deleteTask } = props;
+  const classList = {
+    todo: 'default-todo',
+    doing: 'doing-todo',
+    done: 'finished',
+  };
+  const [hover, setHover] = useState(false);
+  const toggleHover = () => setHover(!hover);
+  const className = classList[state];
+  return (
+    <div
+      className='left-text flex'
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}
+    >
+      <div className={className} id={id} onClick={onClickHandler}>
+        {task}
       </div>
-    );
-  }
-}
+      {hover ? (
+        <CloseButton
+          onClick={() => {
+            deleteTask(id);
+          }}
+        />
+      ) : (
+        ''
+      )}
+    </div>
+  );
+};
 
 export default Task;
